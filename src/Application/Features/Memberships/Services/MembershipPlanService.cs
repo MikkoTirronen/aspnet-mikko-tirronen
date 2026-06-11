c2afdd0060964385688bec95b92e33ffaaf486fe
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-06-11 19:57:12 +0200

    edited features

--- src/Application/Features/Memberships/Services/MembershipPlanService.cs
+++ src/Application/Features/Memberships/Services/MembershipPlanService.cs
@@ -9,9 +9,9 @@ public class MembershipPlanService : IMembershipPlanService
 {
     public static List<MembershipPlan> All =>
     [
-        new() { Name = "Basic", Price = 299, Description = "Perfect for getting started.", Features= ["Gym Access","Locker"] },
-        new() { Name = "Premium", Price = 499, Description = "Most popular plan.", Features=["Gym Access", "Locker", "10st Classes per Month"] },
-        new() { Name = "Elite", Price = 799, Description = "Everything included.", Features=["Gym Access", "Locker",  "Towel and Shower slippers", "Unlimited Classes", "Personal Trainer"] }
+        new() { Name = "Basic", Price = 299, Description = "Perfect for getting started.", Features= ["Gym Access", "Book Classes"] },
+        new() { Name = "Premium", Price = 499, Description = "Most popular plan.", Features=["Gym Access", "Book Classes", "Locker"] },
+        new() { Name = "Elite", Price = 799, Description = "Everything included.", Features=["Gym Access", "Priority Classes Booking", "Locker",  "Towel and Shower slippers", "Personal Trainer"] }
     ];
 
     public List<MembershipPlan> GetPlans()
