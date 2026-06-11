a390ca06047c75d7ad8269d4b0e6c4427faecfc3
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-06-11 12:50:15 +0200

    added a details page and functionality for classes

--- src/Application/Common/Interfaces/IGymClassService.cs
+++ src/Application/Common/Interfaces/IGymClassService.cs
@@ -6,4 +6,5 @@ public interface IGymClassService
 {
     Task<IEnumerable<GymClass>> GetAllAsync(CancellationToken ct);
     Task<GymClass?> GetByIdAsync(Guid id, CancellationToken ct);
+    Task<GymClassDetailsDto?> GetDetailsAsync(Guid id, string userIdString, CancellationToken ct);
 }
--- src/Infrastructure/Services/GymClassService.cs
+++ src/Infrastructure/Services/GymClassService.cs
@@ -21,4 +21,29 @@ public class GymClassService(AppDbContext context) : IGymClassService
     {
         return await _context.GymClasses.FirstOrDefaultAsync(x => x.Id == id, ct);
     }
+
+    public async Task<GymClassDetailsDto?> GetDetailsAsync(Guid id, string userIdString, CancellationToken ct)
+    {
+
+        var userId = Guid.Parse(userIdString);
+
+        var isBooked = await _context.Bookings
+            .AnyAsync(x => x.GymClassId == id && x.UserId == userId, ct);
+        Console.WriteLine(isBooked);
+        return await _context.GymClasses
+            .AsNoTracking()
+            .Where(x => x.Id == id)
+            .Select(x => new GymClassDetailsDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Instructor = x.Instructor,
+                StartTime = x.StartTime,
+                Capacity = x.Capacity,
+                Category = x.Category,
+                BookedCount = _context.Bookings.Count(b => b.GymClassId == x.Id),
+                IsBookedByUser = isBooked
+            })
+            .FirstOrDefaultAsync(ct);
+    }
 }
